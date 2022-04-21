76d30666fa74f977a4116b1292bfcf6ce4c847b1
k <2261933439@qq.com>
2022-04-21 21:16:33 +0800

    first1 commit

--- src/main/java/com/example/gittest01/user/test.java
+++ src/main/java/com/example/gittest01/user/test.java
@@ -3,5 +3,13 @@ package com.example.gittest01.user;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello world!!!!");
+        System.out.println("hello world!!!!");
+        System.out.println("hello world!!!!");
+        System.out.println("hello world");
+        System.out.println("hello world!!!!!!!");
+        System.out.println("hello world!!!!");
+
+
+
     }
 }
